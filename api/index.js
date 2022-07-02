9c0d38515dfcfd6797a7b1f34d551cb76fb20187
Paritosh Chauhan <paritoshchauhan1202@gmail.com>
2022-07-02 20:36:24 +0530

    Some Bug Fixes and Updated Login Route

--- api/index.js
+++ api/index.js
@@ -10,40 +10,42 @@ const categoriesRoute = require("./routes/categories");
 const path = require("path");
 
 dotenv.config();
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use("/images",express.static(path.join(__dirname,"/images")))
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-    })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File is uploaded");
+});
 
-    const storage = multer.diskStorage({
-        destination:(req,file,cb) => {
-            cb(null,"images");
-        },
-        filename:(req,file,cb)=>{
-            cb(null,req.body.name);
-        },
-    });
-
-const upload = multer({storage:storage});
-app.post("/api/upload",upload.single("file"),(req,res)=>{
-    res.status(200).json("File is uploaded");
-})
-
+app.get("/", (req, res) => {
+  res.send("Server is up and running...");
+});
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/post", postRoute);
 app.use("/api/categories", categoriesRoute);
 
 app.listen("5000", () => {
-    console.log("Backend is running.");
+  console.log("Backend is running.");
 });
